32bc3e8d929d6529c055be1e95c76283965aa86d
Muyiwa0 <Muyiwa@ft9ja.com>
2022-11-22 11:14:52 +0100

    fix

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,6 @@
 import '@/styles/tailwind.css'
 import 'focus-visible'
-import 'node_modules/react-modal-video/scss/modal-video.scss';
+// import 'node_modules/react-modal-video/scss/modal-video.scss';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
