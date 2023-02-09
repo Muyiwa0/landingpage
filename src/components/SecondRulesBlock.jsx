f7b20efdceaa4f32fdb0e5007814dac0c14c3f62
Arsalan Ahmad <you@example.com>
2023-02-09 22:44:12 +0500

    Format fixes

--- src/components/SecondRulesBlock.jsx
+++ src/components/SecondRulesBlock.jsx
@@ -30,17 +30,17 @@ const SecondRulesBlock = () => {
           className="mx-auto grid max-w-screen-lg grid-cols-1"
         >
           <div className="mx-auto ">
-            <h1 className="py-7 text-center text-4xl font-bold">
+            <h1 className="py-2 text-center">
               FT9ja Classic
             </h1>
             <div>
-              <h1 className="py-5 text-xl font-semibold">1ST PAYOUT</h1>
+              <h1 className="pb-1 pt-3">1ST PAYOUT</h1>
               <p className="text-normal">
                 You are eligible for the first payout after trading for at least
                 10 days, even if you are on the weekly plan.
               </p>
 
-              <h1 className="py-5 text-xl font-semibold">
+              <h1 className="pb-1 pt-3">
                 2ND CHANCE ACCOUNTS
               </h1>
               <p className="text-normal">
@@ -48,20 +48,20 @@ const SecondRulesBlock = () => {
                 Talent Bonus.
               </p>
 
-              <h1 className="py-5 text-xl font-semibold">PAYOUT DATE</h1>
+              <h1 className="pb-1 pt-3">PAYOUT DATE</h1>
               <p className="text-normal">
                 FT9ja processes payouts on Fridays, even if it&apos;s requested
                 before Friday.
               </p>
 
-              <h1 className="py-5 text-xl font-semibold">PROFIT SPLIT</h1>
+              <h1 className="pb-1 pt-3">PROFIT SPLIT</h1>
               <p className="text-normal">
                 Split percentage in Aso Account is based on when you qualify for
                 Aso Account -- counted from when you place your first trade in
                 the &ldquo;1st chance account&ldquo;.
               </p>
 
-              <h1 className="py-5 text-xl font-semibold">
+              <h1 className="pb-1 pt-3">
                 ONE-TIME FEE REFUND
               </h1>
               <p className="text-normal">
@@ -69,16 +69,16 @@ const SecondRulesBlock = () => {
               </p>
 
               <p>
-                <h1 className="py-10 text-center text-4xl font-bold">
+                <h1 className="py-5 text-center">
                   FT9ja Challenge
                 </h1>
-                <h1 className="py-5 text-xl font-semibold">PAYOUT DATE</h1>
+                <h1 className="pb-1 pt-3">PAYOUT DATE</h1>
                 <p className="text-normal">
                   FT9ja processes payouts on Fridays, even if it&apos;s
                   requested before Friday.
                 </p>
 
-                <h1 className="py-5 text-xl font-semibold">
+                <h1 className="pb-1 pt-3">
                   ONE-TIME FEE REFUND
                 </h1>
                 <p className="text-normal">
